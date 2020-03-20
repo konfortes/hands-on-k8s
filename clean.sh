6217bb4b4502405d2b347869c0098c723ada0803
konfortes <ronen.konfortes@riskified.com>
2020-03-20 21:59:57 +0200

    ingress

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
-
 kubectl config use-context docker-desktop || exit 1
 
 kubectl -n handson delete -f k8s/web
 kubectl -n handson delete -f k8s/user-service
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,11 @@ kubectl -n handson create -f k8s/web
 
 echo "deploying user-service..."
 kubectl -n handson create -f k8s/user-service
+
+echo "deploying nginx ingress controller"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+
+echo "deploying ingress"
+kubectl -n handson apply -f k8s/shared/ingress.yaml
+
