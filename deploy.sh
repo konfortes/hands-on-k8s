a9ffcc8e78248bcc45ee77bad6a2e7b1e0f87cb8
konfortes <ronen.konfortes@riskified.com>
2020-04-12 09:29:03 +0300

    save grafana dashboard

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,22 @@ kubectl config use-context docker-desktop || exit 1
 echo "deploying jaeger"
 kubectl create namespace observability
 set -e
-# kubectl -n handson create -f k8s/shared/jaeger/crd.yaml
-# kubectl -n handson create -f k8s/shared/jaeger/rbac.yaml
-# kubectl -n handson create -f k8s/shared/jaeger/operator.yaml
-# kubectl -n handson create -f k8s/shared/jaeger_cr.yaml
+kubectl -n handson create -f k8s/shared/jaeger/crd.yaml
+kubectl -n handson create -f k8s/shared/jaeger/rbac.yaml
+kubectl -n handson create -f k8s/shared/jaeger/operator.yaml
+kubectl -n handson create -f k8s/shared/jaeger_cr.yaml
+
+
+echo "deploying nginx ingress controller"
+kubectl apply -f k8s/shared/nginx-ingress/mandatory.yaml
+kubectl apply -f k8s/shared/nginx-ingress/service.yaml
+
+kubectl create -f k8s/shared/kube-prometheus/manifests/setup
+until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+kubectl create -f k8s/shared/kube-prometheus/manifests/
 
 echo "deploying web..."
 kubectl -n handson create -f k8s/web
 
 echo "deploying user-service..."
 kubectl -n handson create -f k8s/user-service
-
-echo "deploying nginx ingress controller"
-# kubectl apply -f k8s/shared/nginx-ingress/mandatory.yaml
-# kubectl apply -f k8s/shared/nginx-ingress/service.yaml
-
-# kubectl create -f k8s/shared/kube-prometheus/manifests/setup
-# until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
-# kubectl create -f k8s/shared/kube-prometheus/manifests/
-
